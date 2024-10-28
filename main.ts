80b97d5ffd3e75bd7cd6ff99d5390560018c02a3
DiegoCaroFrechilla <146827294+DiegoCaroFrechilla@users.noreply.github.com>
2024-10-28 16:06:26 +0100

    llego al bloque y me congelo

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
 basic.forever(function () {
-	
+    if (input.temperature() > 22) {
+        basic.showString("calor")
+    } else {
+        basic.showString("frio")
+    }
+})
+basic.forever(function () {
+    if (input.lightLevel() > 200) {
+        music.play(music.stringPlayable("G B A G C5 B A B ", 120), music.PlaybackMode.UntilDone)
+    } else if (input.lightLevel() < 50) {
+        music.play(music.stringPlayable("E B C5 A B G A F ", 120), music.PlaybackMode.UntilDone)
+    } else {
+    	
+    }
 })
